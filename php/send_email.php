9673907ec08059181502de20ec1f87d98b4735cb
garima-l5 <garima.sachdeva@layerfive.com>
2025-02-18 14:34:22 +0530

    mail alert

--- php/send_email.php
+++ php/send_email.php
@@ -65,7 +65,7 @@ if ($API->connect($routerIP, $username, $password, 8736)) {
     $API->read();
     
     // echo "Email sent successfully!";
-    // header("Location: /index.html?status=success");
+    header("Location: /index.html?status=success");
     echo("Your mail has been sent successfully! We will contact you shortly.")
     $API->disconnect();
     exit;
